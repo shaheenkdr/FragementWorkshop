859a69383a367ca3e50e44d3014d0f1f9f35f273
shaheenakader <shaheenkader@outlook.com>
2018-01-16 18:27:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shaheenakader/fragementworkshop/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.shaheenakader.fragementworkshop;
+
+import android.app.Activity;
+
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void selectFrag(View view) {
+        Fragment fr;
+
+        if(view == findViewById(R.id.button2)) {
+            fr = new FragmentTwo();
+
+        }else {
+            fr = new FragmentOne();
+        }
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.commit();
+
+    }
+}
